1f43c4c25bcb37143d615492f810a7185b2edaf9
Hooi Tong <me@hooitong.com>
2014-06-19 21:21:15 +0800

    Add room functionality and revise naming
    Room functionality at the moment is manual and all instance of ‘Pseudo’
    is revised to ‘Nick’ for easier documentation and readability.

--- public/script.js
+++ public/script.js
@@ -1,34 +1,49 @@
 var socket = io.connect();
 
-function addMessage(msg, pseudo) {
-    $("#chatEntries").append('<div class="message"><p>' + pseudo + ' : ' + msg + '</p></div>');
+function addMessage(msg, nick) {
+  $("#chatEntries").append('<div class="message"><p>' + nick + ' : ' + msg + '</p></div>');
 }
 
 function sentMessage(){
-	if($('#messageInput').val() != ""){
-		socket.emit('message', $('#messageInput').val());
-		addMessage($('#messageInput').val(), "Me");
-		$('#messageInput').val('');
-	}
+  if($('#messageInput').val() != ""){
+    socket.emit('message', $('#messageInput').val());
+    addMessage($('#messageInput').val(), "Me");
+    $('#messageInput').val('');
+  }
 }
 
-function setPseudo(){
-	if($("#pseudoInput").val() != ""){
-		socket.emit('setPseudo', $("#pseudoInput").val());
-		$('#chatControls').show();
-		$('#pseudoInput').hide();
-		$('#pseudoSet').hide();
-	}
+function setNick(){
+  if($("#nickInput").val() != ""){
+    socket.emit('setNick', $("#nickInput").val());
+    $('#chatControls').show();
+    $('#nickInput').hide();
+    $('#nickSet').hide();
+  }
+}
+
+function joinRoom(){
+  if($("#roomInput").val() != ""){
+    socket.emit('setRoom', $("#roomInput").val());
+    $("#locationStatus").show();
+    $("#locationStatus").append('You are now in "' + $("#roomInput").val() + '"');
+    $("#roomInput").val() = "";
+  }
 }
 
 socket.on('message', function(data){
-	addMessage(data['message'], data['pseudo']);
+  addMessage(data['message'], data['nick']);
+});
+
+socket.on('adminMessage', function(msg){
+    $("#chatEntries").append('<div class="notification"><p>' + msg + '</p></div>');
 });
 
 $(function() {
-    $("#chatControls").hide();
-    $("#chatEntries").show();
-    $("#chatEntries").append('<div class = "message"><p>Welcome to da club</p></div>');
-    $("#pseudoSet").click(function() {setPseudo()});
-    $("#submit").click(function() {sentMessage()});
+  $("#locationStatus").hide();
+  $("#chatControls").hide();
+  $("#chatEntries").show();
+  $("#chatEntries").append('<div class = "message"><p>Welcome to Cheat Chat.</p></div>');
+  $("#nickSet").click(function() {setNick()});
+  $("#submit").click(function() {sentMessage()});
+  $("#rmJoin").click(function() {joinRoom()});
 });
