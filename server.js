fd433709aaeb5bac410421f6d6033a42b661d355
Hooi Tong <me@hooitong.com>
2014-07-03 15:25:26 +0800

    Post-Milestone 2 Update #1
    Major Release
    - Implemented CDP Headers to help XSS
    - Combined CSS for faster delivery
    - Rearrange, Minify Codes and Scripts for PageSpeed
    - Enable Compression and Resolve Javascript Linting Problems

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ var app = express();
 var server = app.listen(process.env.PORT || 5000);
 var jade = require('jade');
 var chatServer = require('./lib/chat_server').listen(server);
-
+app.use(express.compress());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set("view options", { layout: false });
+
+app.use(function(req, res, next) {
+    if (!res.getHeader('Cache-Control')) res.setHeader('Cache-Control', 'public, max-age=86400000');
+    res.setHeader("content_security_policy", "script-src 'self' https://maps.googleapis.com https://maps.gstatic.com; object-src 'self'");
+    return next();
+});
 
 app.use(express.static(__dirname + '/public'));
 
